1fc014bf2fa81eb41da8d23efbd0e7dfa7f8d171
Ikehkizito <ikehkizito09@gmail.com>
2025-08-03 21:52:54 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+name = input ("your username:")
+password = input ("password:")
+if name == "Ikeh kizito" and password == "Ikehkizito@2007":
+    print (" welcome back ikeh")
+elif name == "Ikehkizito" and password !=  "Ikehkizito@2007":
+    print ("password incorrect")
+else:
+    print ("⚠️access denied⚠️")
